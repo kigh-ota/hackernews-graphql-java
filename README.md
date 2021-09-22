0a9b7ecc99d6b181a55d67f055b06170320ad517
Kaiichiro Ota <kaiichiro-ota@cybozu.co.jp>
2021-09-22 14:00:56 +0900

    initial commit: finished Queries

---
+++ README.md
@@ -0,0 +1,3 @@
+# HOW TO GRAPHQL: graphql-java Tutorial
+
+https://www.howtographql.com/graphql-java/0-introduction/
---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,25 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+import org.jetbrains.annotations.NotNull;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    @NotNull
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
